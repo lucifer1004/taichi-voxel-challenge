d6bae498dc6eea8ee59ce4eaec7ac49e0744f510
Gabriel Wu <wuzihua@pku.edu.cn>
2022-05-18 14:50:25 +0800

    update scene04

--- gallery/scene04.py
+++ gallery/scene04.py
@@ -2,15 +2,15 @@ from scene import Scene, MAT_LAMBERTIAN, MAT_LIGHT
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(exposure=1, voxel_edges=0.0)
+scene = Scene(exposure=1, voxel_edges=0.01)
 scene.set_floor(0.0, (1.0, 1.0, 1.0))
 scene.set_background_color((1.0, 1.0, 1.0))
 scene.set_directional_light((0.5, 0.5, 0.5),
-                            0.02, (1.0, 1.0, 1.0))
+                            0.05, (0.2, 0.2, 0.2))
 
 
 @ti.func
-def random_walk(xl, xu, yl, yu, zl, zu, rl, ru, gl, gu, bl, bu, mat, num=1000):
+def random_walk(xl, xu, yl, yu, zl, zu, rl, ru, gl, gu, bl, bu, num=1000):
     x = (xl + xu) // 2
     y = (yl + yu) // 2
     z = (zu + zu) // 2
@@ -47,20 +47,23 @@ def random_walk(xl, xu, yl, yu, zl, zu, rl, ru, gl, gu, bl, bu, mat, num=1000):
             b = b - 1 if b - 1 >= bl else bu
         else:
             b = b + 1 if b + 1 <= bu else bl
+
+        mat = MAT_LIGHT if (11 * x + 3 * y + 5 *
+                            z) % 7 == 0 else MAT_LAMBERTIAN
         scene.set_voxel(vec3(x, y, z), mat, vec3(r, g, b) * 0.01)
 
 
 @ ti.kernel
 def initialize_voxels():
-    random_walk(-64, 63, 0, 0, -64, 63, 20, 40, 60,
-                100, 20, 40, MAT_LAMBERTIAN, 100000)
+    random_walk(-63, 63, 0, 0, -63, 63, 20, 40, 60,
+                100, 20, 40, 100000)
     random_walk(-25, 25, 1, 51, -25, 25, 0, 100, 0,
-                100, 0, 100, MAT_LAMBERTIAN, 50000)
+                100, 0, 100, 50000)
     for x in range(-64, 64):
         for y in range(-64, 64):
             _, color = scene.get_voxel(vec3(x, 0, y))
             if color[1] < 0.6:
-                scene.set_voxel(vec3(x, 0, y), MAT_LIGHT, vec3(
+                scene.set_voxel(vec3(x, 0, y), MAT_LAMBERTIAN, vec3(
                     ti.random() * 0.2 + 0.1, ti.random() * 0.2 + 0.1, ti.random() * 0.2 + 0.8))
 
 
--- main.py
+++ main.py
@@ -1,39 +1,70 @@
-from scene import Scene, MAT_LAMBERTIAN
+from scene import Scene, MAT_LAMBERTIAN, MAT_LIGHT
 import taichi as ti
 from taichi.math import *
 
-scene = Scene(exposure=1, voxel_edges=0.0)
-scene.set_floor(-50, (1.0, 1.0, 1.0))
+scene = Scene(exposure=1, voxel_edges=0.01)
+scene.set_floor(0.0, (1.0, 1.0, 1.0))
 scene.set_background_color((1.0, 1.0, 1.0))
 scene.set_directional_light((0.5, 0.5, 0.5),
-                            0.02, (1.0, 1.0, 1.0))
-
-GREEN = vec3(0.0, 1.0, 0.0)
-WHITE = vec3(1.0, 1.0, 1.0)
-OUTER = (56, 26)
-INNER = (38, 16)
+                            0.05, (0.2, 0.2, 0.2))
 
 
 @ti.func
-def should_be_white(i, j):
-    return (abs(i) <= OUTER[0] and abs(j) <= OUTER[1]) and (abs(i) == OUTER[0] or i == 0 or (9 < ti.sqrt(i ** 2 + j ** 2) <= 10) or abs(j) == OUTER[1] or (abs(i) == INNER[0] and abs(j) <= INNER[1]) or (abs(i) >= INNER[0] and abs(j) == INNER[1]) or (4 < ti.sqrt((i - OUTER[0]) ** 2 + (j - OUTER[1]) ** 2) <= 5) or (4 < ti.sqrt((i + OUTER[0]) ** 2 + (j - OUTER[1]) ** 2) <= 5) or (4 < ti.sqrt((i - OUTER[0]) ** 2 + (j + OUTER[1]) ** 2) <= 5) or (4 < ti.sqrt((i + OUTER[0]) ** 2 + (j + OUTER[1]) ** 2) <= 5))
+def random_walk(xl, xu, yl, yu, zl, zu, rl, ru, gl, gu, bl, bu, num=1000):
+    x = (xl + xu) // 2
+    y = (yl + yu) // 2
+    z = (zu + zu) // 2
+    r = (rl + ru) // 2
+    g = (gl + gu) // 2
+    b = (bl + bu) // 2
+
+    ti.loop_config(serialize=True)
+    for _ in range(num):
+        v = ti.random(int) % 6
+        if v == 0:
+            x = x - 1 if x - 1 >= xl else xu
+        elif v == 1:
+            x = x + 1 if x + 1 <= xu else xl
+        elif v == 2:
+            y = y - 1 if y - 1 >= yl else yu
+        elif v == 3:
+            y = y + 1 if y + 1 <= yu else yl
+        elif v == 4:
+            z = z - 1 if z - 1 >= zl else zu
+        else:
+            z = z + 1 if z + 1 <= zu else zl
+
+        v = ti.random(int) % 6
+        if v == 0:
+            r = r - 1 if r - 1 >= rl else ru
+        elif v == 1:
+            r = r + 1 if r + 1 <= ru else rl
+        elif v == 2:
+            g = g - 1 if g - 1 >= gl else gu
+        elif v == 3:
+            g = g + 1 if g + 1 <= gu else gl
+        elif v == 4:
+            b = b - 1 if b - 1 >= bl else bu
+        else:
+            b = b + 1 if b + 1 <= bu else bl
+
+        mat = MAT_LIGHT if (11 * x + 3 * y + 5 *
+                            z) % 7 == 0 else MAT_LAMBERTIAN
+        scene.set_voxel(vec3(x, y, z), mat, vec3(r, g, b) * 0.01)
 
 
-@ti.kernel
+@ ti.kernel
 def initialize_voxels():
-    for i in range(-63, 64):
-        for j in range(-31, 32):
-            color = WHITE if should_be_white(i, j) else GREEN
-            scene.set_voxel(vec3(i, 0, j), MAT_LAMBERTIAN, color)
-
-    for i in range(OUTER[0], 64):
-        for j in range(9):
-            for k in range(10):
-                if (abs(j) == 8 or k == 9 or abs(i) == 63) and (i + j + k) % 2 == 0:
-                    scene.set_voxel(vec3(i, k, j), MAT_LAMBERTIAN, WHITE)
-                    scene.set_voxel(vec3(-i, k, j), MAT_LAMBERTIAN, WHITE)
-                    scene.set_voxel(vec3(i, k, -j), MAT_LAMBERTIAN, WHITE)
-                    scene.set_voxel(vec3(-i, k, -j), MAT_LAMBERTIAN, WHITE)
+    random_walk(-63, 63, 0, 0, -63, 63, 20, 40, 60,
+                100, 20, 40, 100000)
+    random_walk(-25, 25, 1, 51, -25, 25, 0, 100, 0,
+                100, 0, 100, 50000)
+    for x in range(-64, 64):
+        for y in range(-64, 64):
+            _, color = scene.get_voxel(vec3(x, 0, y))
+            if color[1] < 0.6:
+                scene.set_voxel(vec3(x, 0, y), MAT_LAMBERTIAN, vec3(
+                    ti.random() * 0.2 + 0.1, ti.random() * 0.2 + 0.1, ti.random() * 0.2 + 0.8))
 
 
 initialize_voxels()
